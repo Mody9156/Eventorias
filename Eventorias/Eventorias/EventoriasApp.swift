a2c52fce6fe7352bf48dfb801365048aef405634
Modibo KEITA <modykeita2@gmail.com>
2024-11-25 18:43:12 +0100

    created new projet Xcode

---
+++ Eventorias/Eventorias/EventoriasApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventoriasApp.swift
+//  Eventorias
+//
+//  Created by KEITA on 25/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct EventoriasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
