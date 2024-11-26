b3f85df02699970ce348c9a5a2225edec3cc2850
Modibo KEITA <modykeita2@gmail.com>
2024-11-26 14:22:53 +0100

    update text and add zstack and color

--- Eventorias/Eventorias/View/ContentView.swift
+++ Eventorias/Eventorias/View/ContentView.swift
@@ -9,13 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.gray.ignoresSafeArea()
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("EVENTORIAS")
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
