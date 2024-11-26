17fbddc19abfa50dc1c0fc6611eee80069b65b7f
Modibo KEITA <modykeita2@gmail.com>
2024-11-26 12:43:56 +0100

    add new var

--- Eventorias/Eventorias/EventoriasApp.swift
+++ Eventorias/Eventorias/EventoriasApp.swift
@@ -20,7 +20,7 @@ class AppDelegate : NSObject, UIApplicationDelegate {
 
 @main
 struct EventoriasApp: App {
-    
+    @UIApplicationDelegateAdaptor (AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             NavigationStack {
