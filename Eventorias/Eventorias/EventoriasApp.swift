309be77461993b6c7746f48b3bf89c60777de0cf
Modibo KEITA <modykeita2@gmail.com>
2024-11-26 14:37:42 +0100

    created new view

--- Eventorias/Eventorias/EventoriasApp.swift
+++ Eventorias/Eventorias/EventoriasApp.swift
@@ -9,11 +9,13 @@ import SwiftUI
 import FirebaseCore
 import FirebaseFirestore
 import FirebaseAuth
+import FirebaseAuthUI
 
 class AppDelegate : NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         
         FirebaseApp.configure()
+
         return true
     }
 }
---
+++ Eventorias/Eventorias/View/AuthenficiationView.swift
@@ -0,0 +1,21 @@
+//
+//  AuthenficiationView.swift
+//  Eventorias
+//
+//  Created by KEITA on 26/11/2024.
+//
+
+import SwiftUI
+
+struct AuthenficiationView: View {
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct AuthenficiationView_Previews: PreviewProvider {
+    static var previews: some View {
+        AuthenficiationView()
+    }
+}
