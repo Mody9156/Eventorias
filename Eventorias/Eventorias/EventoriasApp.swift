c0e59a701d048872706168f03e888cdc61aa0da0
Modibo KEITA <modykeita2@gmail.com>
2024-11-26 12:41:44 +0100

    add new class and configue farebaeApp

--- Eventorias/Eventorias/EventoriasApp.swift
+++ Eventorias/Eventorias/EventoriasApp.swift
@@ -10,11 +10,22 @@ import FirebaseCore
 import FirebaseFirestore
 import FirebaseAuth
 
+class AppDelegate : NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        FirebaseApp.configure()
+        return true
+    }
+}
+
 @main
 struct EventoriasApp: App {
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack {
+                ContentView()
+            }
         }
     }
 }
