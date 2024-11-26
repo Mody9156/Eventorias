25dbda6113cae8b0ab7970d99dcd0884362769ed
Modibo KEITA <modykeita2@gmail.com>
2024-11-26 14:31:09 +0100

    add new view

---
+++ Eventorias/Eventorias/View/ListView.swift
@@ -0,0 +1,20 @@
+//
+//  ListView.swift
+//  Eventorias
+//
+//  Created by KEITA on 26/11/2024.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView()
+    }
+}
