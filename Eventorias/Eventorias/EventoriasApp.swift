63ac404c47f26532fa553ddc33b11c033f85baca
Modibo KEITA <modykeita2@gmail.com>
2024-11-26 16:14:20 +0100

    initialise settings

--- Eventorias/Eventorias/EventoriasApp.swift
+++ Eventorias/Eventorias/EventoriasApp.swift
@@ -14,9 +14,16 @@ class AppDelegate : NSObject, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         
         FirebaseApp.configure()
+        
         if let emulatorHost = ProcessInfo.processInfo.environment["FIREBASE_AUTH_EMULATOR_HOST"] {
                     Auth.auth().useEmulator(withHost:"localhost", port: 9099)
                 }
+        
+        let settings = Firestore.firestore().settings
+        settings.host = "localhost:8080"
+        settings.isSSLEnabled = false
+        Firestore.firestore().settings = settings
+        
         return true
     }
 }
