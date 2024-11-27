385e13d0aa03c9b8988644dc75a486a09bda7e96
Modibo KEITA <modykeita2@gmail.com>
2024-11-27 10:45:09 +0100

    add comment

--- Eventorias/Eventorias/ViewModel/AuthentificationViewModel.swift
+++ Eventorias/Eventorias/ViewModel/AuthentificationViewModel.swift
@@ -7,21 +7,30 @@
 
 import Foundation
 import FirebaseAuth
+import FirebaseFirestore
+import FirebaseCore
 
 class AuthentificationViewModel : ObservableObject {
     @Published var errorMessage: String? = nil
 
     func login(email : String){
+        guard !email.isEmpty else {
+                   self.errorMessage = "L'email ou le mot de passe ne peuvent pas être vides."
+            
+            print("L'email ou le mot de passe ne peuvent pas être vides.")
+                   return
+               }
         
-        Auth.auth().fetchSignInMethods(forEmail: email){ result , error in
-            if let error = error  {
-                self.errorMessage = error.localizedDescription
-                print("Voici votre erreur : \(self.errorMessage ?? "Erreur inconnue")")
-            }else{
-                self.errorMessage = nil
-                print("L'email est déjà enregistré, vous pouvez vous connecter.")
-            }
-        }
+//        Auth.auth().signIn(withCustomToken: <#T##String#>){ user , error in
+//            if let error = error  {
+//                self.errorMessage = error.localizedDescription
+//                print("Voici votre erreur : \(self.errorMessage ?? "Erreur inconnue")")
+//            }else{
+//                self.errorMessage = nil
+//                print("L'email est déjà enregistré, vous pouvez vous connecter.\(String(describing: result))")
+//            }
+            
+//        }
     }
     
     func registerUser(email:String,password:String){
@@ -39,8 +48,6 @@ class AuthentificationViewModel : ObservableObject {
                 self.errorMessage = nil
                 print("Utilisateur créé avec succès!")
             }
-            
         }
     }
-    
 }
