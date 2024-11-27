3a68e09d8ecdb391bfd0d0cfb3633e7577a64c60
Modibo KEITA <modykeita2@gmail.com>
2024-11-27 11:13:55 +0100

    update function login and add new published

--- Eventorias/Eventorias/ViewModel/AuthentificationViewModel.swift
+++ Eventorias/Eventorias/ViewModel/AuthentificationViewModel.swift
@@ -12,23 +12,25 @@ import FirebaseCore
 
 class AuthentificationViewModel : ObservableObject {
     @Published var errorMessage: String? = nil
-
+    @Published var isAuthenticated : Bool = false
+    
     func login(email : String,password:String){
-        guard !email.isEmpty else {
-                   self.errorMessage = "L'email ou le mot de passe ne peuvent pas être vides."
-            
-            print("L'email ou le mot de passe ne peuvent pas être vides.")
+        //Validation du mail et du mot de passe
+        guard !email.isEmpty, !password.isEmpty else {
+                   self.errorMessage = "Veuillez remplir tout les champs."
+            print(String(describing:errorMessage))
                    return
                }
+        //Connexion avec Firebase
         Auth.auth().signIn(withEmail: email, password: password){ result , error in
-            if result != nil && self.errorMessage == nil {
-               print("L'email est déjà enregistré, vous pouvez vous connecter.\(String(describing: result))")
+            if let error = error {
+                // Gestion des erreurs de connexion
+                self.errorMessage = error.localizedDescription
+                self.isAuthenticated = false
+            } else {
+                // Connexion réussie
                 self.errorMessage = nil
-            }else{
-                if let error = error  {
-                    self.errorMessage = error.localizedDescription
-                    print("Voici votre erreur : \(self.errorMessage ?? "Erreur inconnue")")
-                }
+                self.isAuthenticated = true
             }
         }
     }
@@ -39,7 +41,7 @@ class AuthentificationViewModel : ObservableObject {
                    self.errorMessage = "L'email ou le mot de passe ne peuvent pas être vides."
                    return
                }
-        
+        //Création d'un compte utilisateur
         Auth.auth().createUser(withEmail: email, password: password){ result , error in
             if let error = error  {
                 self.errorMessage = error.localizedDescription
