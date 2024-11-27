b50537d218bf2ac6d5bc705b491ee8de8698350a
Modibo KEITA <modykeita2@gmail.com>
2024-11-27 18:02:44 +0100

    update struct

--- Eventorias/Eventorias/Model/EventEntry.swift
+++ Eventorias/Eventorias/Model/EventEntry.swift
@@ -6,7 +6,22 @@
 //
 
 import Foundation
+import FirebaseFirestoreSwift
 
-struct EventEntry {
+struct EventEntry : Identifiable, Codable{
+    @DocumentID // lier automatiquement un champ d'une structure à l'ID d'un document Firestore.
+    var id : String?
+    var picture : String
+    var title : String
+    var dateString : String
+    var poster : String
     
+}
+
+extension EventEntry {
+    static let eventEntry  = [
+        EventEntry(picture: "", title: "", dateString: "", poster: ""),
+        EventEntry(picture: "", title: "", dateString: "", poster: ""),
+        EventEntry(picture: "", title: "", dateString: "", poster: "")
+    ]
 }
