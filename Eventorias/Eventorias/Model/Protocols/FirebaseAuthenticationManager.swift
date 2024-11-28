338df4521ab1e3ab9757d4bc40b0f6497fd25dde
Modibo KEITA <modykeita2@gmail.com>
2024-11-28 13:04:50 +0100

    use completion

--- Eventorias/Eventorias/Model/Protocols/FirebaseAuthenticationManager.swift
+++ Eventorias/Eventorias/Model/Protocols/FirebaseAuthenticationManager.swift
@@ -6,7 +6,30 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
+import FirebaseCore
 
 class FirebaseAuthenticationManager :protocolsFirebaseData {
+    
+    func signIn(email: String, password: String, completion: @escaping (Result<Any, Error>) -> Void) {
+        Auth.auth().signIn(withEmail: email, password: password){ result , error in
+            if let error = error {
+                // Gestion des erreurs de connexion
+
+                completion(.failure(error))
+            } else {
+                // Connexion réussie
+                self.errorMessage = nil
+                self.isAuthenticated = true
+                print("Graduation")
+            }
+        }
+    }
+    
+    func createUser(email: String, password: String, completion: @escaping (Result<Any, Error>) -> Void) {
+        <#code#>
+    }
+    
     
 }
