bee2f18b529e462c137add34e68274968b7b275f
Modibo KEITA <modykeita2@gmail.com>
2024-11-28 11:02:57 +0100

    update protocol

--- Eventorias/Eventorias/Model/Protocols/protocolsFirebaseData.swift
+++ Eventorias/Eventorias/Model/Protocols/protocolsFirebaseData.swift
@@ -8,6 +8,6 @@
 import Foundation
 
 protocol protocolsFirebaseData{
-    func login(email : String,password:String)
+    func signIn(email : String, password:String, completion:@escaping (Result<Any, Error>)-> Void)
     func registerUser(email:String,password:String)
 }
