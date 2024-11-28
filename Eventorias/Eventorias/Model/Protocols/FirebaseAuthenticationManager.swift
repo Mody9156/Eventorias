3e0ab850ed847632d098a80ff9209dc5974086f4
Modibo KEITA <modykeita2@gmail.com>
2024-11-28 12:14:18 +0100

    add new file

---
+++ Eventorias/Eventorias/Model/Protocols/FirebaseAuthenticationManager.swift
@@ -0,0 +1,12 @@
+//
+//  FirebaseAuthenticationManager.swift
+//  Eventorias
+//
+//  Created by KEITA on 28/11/2024.
+//
+
+import Foundation
+
+class FirebaseAuthenticationManager :protocolsFirebaseData {
+    
+}
--- Eventorias/Eventorias/Model/Protocols/protocolsFirebaseData.swift
+++ Eventorias/Eventorias/Model/Protocols/protocolsFirebaseData.swift
@@ -8,6 +8,6 @@
 import Foundation
 
 protocol protocolsFirebaseData{
-    func signIn(email : String, password:String, completion:@escaping (Result<Any, Error>)-> Void)
-    func registerUser(email:String,password:String)
+    func signIn(email : String, password:String, completion:@escaping(Result<Any, Error>)-> Void)
+    func createUser(email:String, password:String, completion:@escaping(Result<Any,Error>)-> Void)
 }
