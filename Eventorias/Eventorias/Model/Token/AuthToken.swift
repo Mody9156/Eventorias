a5447e30ea1dfe712f0fa7a7aa5ae076f02fb006
Modibo KEITA <modykeita2@gmail.com>
2024-11-29 10:12:18 +0100

    add new function

--- Eventorias/Eventorias/Model/Token/AuthToken.swift
+++ Eventorias/Eventorias/Model/Token/AuthToken.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+struct AuthToken {
+    private static let tokenKey = "authToken"
+    
+    static func addToken(_ token :String ) -> String {
+        UserDefaults.standard.set(token, forKey: tokenKey)
+        return token
+    }
+    
+    static func retrieve() -> String?{
+        return UserDefaults.standard.string(forKey: tokenKey)
+    }
+}
