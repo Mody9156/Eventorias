701ae406bbb932c64939b064ec4423b53cf7fe23
Modibo KEITA <modykeita2@gmail.com>
2024-12-02 16:38:23 +0100

    use extension

--- Eventorias/Eventorias/Model/Extension/DateExtension.swift
+++ Eventorias/Eventorias/Model/Extension/DateExtension.swift
@@ -7,10 +7,12 @@
 
 import Foundation
 
-func dateFromString(_ isoString : String)-> Date? {
-    let isoDateFormatter = ISO8601DateFormatter()
-    isoDateFormatter.formatOptions = [.withFullDate]
-    
-    return isoDateFormatter.date(from: isoString)
+extension Date {
+    static func dateFromString(_ isoString : String)-> Date? {
+        let isoDateFormatter = ISO8601DateFormatter()
+        isoDateFormatter.formatOptions = [.withFullDate]
+        
+        return isoDateFormatter.date(from: isoString)
+    }
     
 }
