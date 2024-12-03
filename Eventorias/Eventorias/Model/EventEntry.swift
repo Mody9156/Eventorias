dd328541724b9aef6504020c0fcaa3bae920d10f
Modibo KEITA <modykeita2@gmail.com>
2024-12-03 19:27:20 +0100

    update array

--- Eventorias/Eventorias/Model/EventEntry.swift
+++ Eventorias/Eventorias/Model/EventEntry.swift
@@ -15,18 +15,18 @@ struct EventEntry : Identifiable, Codable,Hashable{
     var title : String
     var dateCreationString : String
     var dateCreation : Date {
-        guard let date = Date.dateFromString(dateCreationString) else{
-            return Date.now
-        }
-        return  date
+    Date.dateFromString(dateCreationString) ??  Date.now
     }
     var poster : String
 }
-
 extension EventEntry {
     static let eventEntry  = [
-        EventEntry(picture: "MusicFestival", title: "Music festival", dateCreationString: "June 15, 2024", poster: "MusicFestivalPoster"),
+        EventEntry(picture: "MusicFestival", title: "Music festival", dateCreationString: "2024-06-15T12:00:00Z", poster: "MusicFestivalPoster"),
         EventEntry(picture: "ArtExhibition", title: "Art exhibition", dateCreationString: "July 20, 2024", poster: "ArtExhibitionPoster"),
-        EventEntry(picture: "TechConference", title: "Tech conference", dateCreationString: "August 5, 2024", poster: "TechConferencePoster")
+        EventEntry(picture: "TechConference", title: "Tech conference", dateCreationString: "August 5, 2024", poster: "TechConferencePoster"),
+        EventEntry(picture: "FoodFaire", title: "Food fair", dateCreationString: "September 12, 2024", poster: "FoodFairePoster"),
+        EventEntry(picture: "BookSigning", title: "Book signing", dateCreationString: "October 3, 2024", poster: "BookSigningPoster"),
+        EventEntry(picture: "FilmScreening", title: "Book signing", dateCreationString: "November 10, 2024", poster: "FilmScreeningPoster.png"),
+        EventEntry(picture: "CharityRun", title: "Charity run", dateCreationString: "Octobre 10, 2024", poster: "CharityRunPoster")
     ]
 }
