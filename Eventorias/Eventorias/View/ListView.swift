0b13d7a47d69ea3dd3afd00bd1f1b2cfc80be4ed
Modibo KEITA <modykeita2@gmail.com>
2024-12-03 18:22:05 +0100

    update git status

--- Eventorias/Eventorias/View/ListView.swift
+++ Eventorias/Eventorias/View/ListView.swift
@@ -88,13 +88,13 @@ struct ListView: View {
                     }
                     .toolbar(content: myTollBarContent)
                                     }
-                //                ZStack {
-                //                    RoundedRectangle(cornerRadius: 16)
-                //                      .fill(.red)
-                //                      .frame(width: 56, height: 56)
-                //                       Image(systemName: "plus")
-                //                         .foregroundColor(.white)
-                //                    }
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 16)
+                                      .fill(.red)
+                                      .frame(width: 56, height: 56)
+                                       Image(systemName: "plus")
+                                         .foregroundColor(.white)
+                                    }
             }
             Spacer()
             
