d29ada0de4e0e30763c6831529542a7b9590dfc8
Modibo KEITA <modykeita2@gmail.com>
2024-12-05 12:12:41 +0100

    add new ZStack and deplace button

--- Eventorias/Eventorias/View/ListView.swift
+++ Eventorias/Eventorias/View/ListView.swift
@@ -29,72 +29,76 @@ struct ListView: View {
                         CustomButton(listViewModel: listViewModel, tryEvent: $tryEvent)
                             .padding()
                         
-                        List {
-                            Section {
-                                ForEach(filter,id: \.self) { entry in
-                                   
-                                        HStack {
-                                            Image(entry.picture)
-                                                .resizable()
-                                                .frame(width: 40,height: 40)
-                                                .padding()
-                                            
-                                            VStack(alignment:.leading){
-                                                Text(entry.title)
-                                                    .font(.custom("Inter-Medium", size: 16))
-                                                    .lineSpacing(24 - 16)
-                                                    .fontWeight(.medium)
-                                                    .multilineTextAlignment(.leading)
-                                                    .foregroundColor(.white)
+                        ZStack {
+                            List {
+                                Section {
+                                    ForEach(filter,id: \.self) { entry in
+                                       
+                                            HStack {
+                                                Image(entry.picture)
+                                                    .resizable()
+                                                    .frame(width: 40,height: 40)
+                                                    .padding()
                                                 
-                                                Text("\(listViewModel.formatDateString(eventEntry: eventEntry))")
-                                                    .font(.custom("Inter-Regular", size: 14))
-                                                    .lineSpacing(20 - 14)
-                                                    .fontWeight(.regular)
-                                                    .multilineTextAlignment(.leading)
-                                                    .foregroundColor(.white)
-                                            }
+                                                VStack(alignment:.leading){
+                                                    Text(entry.title)
+                                                        .font(.custom("Inter-Medium", size: 16))
+                                                        .lineSpacing(24 - 16)
+                                                        .fontWeight(.medium)
+                                                        .multilineTextAlignment(.leading)
+                                                        .foregroundColor(.white)
+                                                    
+                                                    Text("\(listViewModel.formatDateString(eventEntry: eventEntry))")
+                                                        .font(.custom("Inter-Regular", size: 14))
+                                                        .lineSpacing(20 - 14)
+                                                        .fontWeight(.regular)
+                                                        .multilineTextAlignment(.leading)
+                                                        .foregroundColor(.white)
+                                                }
+                                                
+                                                Spacer()
+                                                Image(entry.poster)
+                                                    .resizable()
+                                                    .frame(width: 136, height: 80)
+                                                    .cornerRadius(12)
                                             
-                                            Spacer()
-                                            Image(entry.poster)
-                                                .resizable()
-                                                .frame(width: 136, height: 80)
-                                                .cornerRadius(12)
-                                        
-                                        }.overlay(NavigationLink(destination: {
-                                            UserDetailView(eventEntry: entry)
-                                        }, label: {
-                                            EmptyView()
-                                        }))
+                                            }.overlay(NavigationLink(destination: {
+                                                UserDetailView(eventEntry: entry)
+                                            }, label: {
+                                                EmptyView()
+                                            }))
+                                    }
+                                   
                                 }
-                               
-                            }
-                            .listRowBackground(
-                                RoundedRectangle(cornerRadius:16)
-                                    .fill(Color("BackgroundDocument"))
-                                    .frame(width: 358, height: 80)
+                                .listRowBackground(
+                                    RoundedRectangle(cornerRadius:16)
+                                        .fill(Color("BackgroundDocument"))
+                                        .frame(width: 358, height: 80)
 
-                                    .padding(2)
-                            )
-                            
-                        }
-                        .listStyle(GroupedListStyle())
-                        .scrollContentBackground(.hidden)
-                        .background(Color("Background"))
-                        
-                        HStack {
-                            Spacer()
-                            Button(action:{}) {
-                                ZStack {
-                                    RoundedRectangle(cornerRadius: 16)
-                                      .fill(.red)
-                                      .frame(width: 56, height: 56)
-                                       Image(systemName: "plus")
-                                         .foregroundColor(.white)
-                                }
+                                        .padding(2)
+                                )
+                                
                             }
-                            .padding()
+                            .listStyle(GroupedListStyle())
+                            .scrollContentBackground(.hidden)
+                        .background(Color("Background"))
+                            
+                            HStack {
+                                Spacer()
+                                Button(action:{}) {
+                                    ZStack {
+                                        RoundedRectangle(cornerRadius: 16)
+                                          .fill(.red)
+                                          .frame(width: 56, height: 56)
+                                           Image(systemName: "plus")
+                                             .foregroundColor(.white)
+                                    }
+                                }
+                                .padding()
+                            }
                         }
+                        
+                       
 
                     }
                     .toolbar(content: myTollBarContent)
