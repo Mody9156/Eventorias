47785488f7f310363a5c388f89c1fe7737ca28e7
Modibo KEITA <modykeita2@gmail.com>
2024-12-06 18:46:23 +0100

    replace button ith menu

--- Eventorias/Eventorias/View/ListView.swift
+++ Eventorias/Eventorias/View/ListView.swift
@@ -178,26 +178,10 @@ struct CustomButton: View {
     @StateObject var listViewModel : ListViewModel
     @Binding var tryEvent : Bool
     var body: some View {
-        Button(action:{
-            
-        }){
-            ZStack {
-                Rectangle()
-                    .frame(width: 105, height: 35)
-                    .cornerRadius(20)
-                    .foregroundColor(Color("BackgroundDocument"))
-                
-                HStack(alignment: .top){
-                    Image("Sort")
-                        .resizable()
-                        .frame(width: 12, height: 16)
-                    
-                    Text("Sorting")
-                        .foregroundColor(.white)
-                }
-                .padding(.horizontal)
-            }
+        
+        Menu("Sorting") {
         }
+        
     }
 }
 
