15c34478e19f4cee68bed8fa9429ee106753e7ff
Modibo KEITA <modykeita2@gmail.com>
2024-12-06 10:54:12 +0100

    deplace ZStack

--- Eventorias/Eventorias/View/ListView.swift
+++ Eventorias/Eventorias/View/ListView.swift
@@ -20,92 +20,93 @@ struct ListView: View {
     
     var body: some View {
         NavigationStack {
-            VStack {
-                ZStack(alignment: .leading) {
-                    Color("Background")
-                        .ignoresSafeArea()
-                    
-                    VStack(alignment: .leading) {
-                        CustomButton(listViewModel: listViewModel, tryEvent: $tryEvent)
-                            .padding()
+            ZStack (alignment: .leading) {
+                Color("Background")
+                    .ignoresSafeArea()
+                VStack {
+                
                         
-                        ZStack(alignment: .bottomTrailing) {
-                            List {
-                                Section {
-                                    ForEach(filter,id: \.self) { entry in
-                                        
-                                        HStack {
-                                            Image(entry.picture)
-                                                .resizable()
-                                                .frame(width: 40,height: 40)
-                                                .padding()
+                        VStack(alignment: .leading) {
+                            CustomButton(listViewModel: listViewModel, tryEvent: $tryEvent)
+                                .padding()
+                            
+                            ZStack(alignment: .bottomTrailing) {
+                                List {
+                                    Section {
+                                        ForEach(filter,id: \.self) { entry in
                                             
-                                            VStack(alignment:.leading){
-                                                Text(entry.title)
-                                                    .font(.custom("Inter-Medium", size: 16))
-                                                    .lineSpacing(24 - 16)
-                                                    .fontWeight(.medium)
-                                                    .multilineTextAlignment(.leading)
-                                                    .foregroundColor(.white)
+                                            HStack {
+                                                Image(entry.picture)
+                                                    .resizable()
+                                                    .frame(width: 40,height: 40)
+                                                    .padding()
                                                 
-                                                Text("\(listViewModel.formatDateString( entry.dateCreation))")
-                                                    .font(.custom("Inter-Regular", size: 14))
-                                                    .lineSpacing(20 - 14)
-                                                    .fontWeight(.regular)
-                                                    .multilineTextAlignment(.leading)
+                                                VStack(alignment:.leading){
+                                                    Text(entry.title)
+                                                        .font(.custom("Inter-Medium", size: 16))
+                                                        .lineSpacing(24 - 16)
+                                                        .fontWeight(.medium)
+                                                        .multilineTextAlignment(.leading)
+                                                        .foregroundColor(.white)
+                                                    
+                                                    Text("\(listViewModel.formatDateString( entry.dateCreation))")
+                                                        .font(.custom("Inter-Regular", size: 14))
+                                                        .lineSpacing(20 - 14)
+                                                        .fontWeight(.regular)
+                                                        .multilineTextAlignment(.leading)
+                                                        .foregroundColor(.white)
+                                                }
+                                                
+                                                Spacer()
+                                                Image(entry.poster)
+                                                    .resizable()
+                                                    .frame(width: 136, height: 80)
+                                                    .cornerRadius(12)
+                                                
+                                            }.overlay(NavigationLink(destination: {
+                                                UserDetailView(eventEntry: entry)
+                                            }, label: {
+                                                EmptyView()
+                                            }))
+                                        }
+                                        
+                                    }
+                                    .listRowBackground(
+                                        RoundedRectangle(cornerRadius:16)
+                                            .fill(Color("BackgroundDocument"))
+                                            .frame(width: 358, height: 80)
+                                        
+                                            .padding(2)
+                                    )
+                                    
+                                }
+                                .listStyle(GroupedListStyle())
+                                .scrollContentBackground(.hidden)
+                                .background(Color("Background"))
+                                
+                                ZStack(alignment:.bottomTrailing) {
+                                    HStack{
+                                        Spacer()
+                                        Button(action:{}) {
+                                            ZStack {
+                                                RoundedRectangle(cornerRadius: 16)
+                                                    .fill(.red)
+                                                    .frame(width: 56, height: 56)
+                                                Image(systemName: "plus")
                                                     .foregroundColor(.white)
                                             }
-                                            
-                                            Spacer()
-                                            Image(entry.poster)
-                                                .resizable()
-                                                .frame(width: 136, height: 80)
-                                                .cornerRadius(12)
-                                            
-                                        }.overlay(NavigationLink(destination: {
-                                            UserDetailView(eventEntry: entry)
-                                        }, label: {
-                                            EmptyView()
-                                        }))
-                                    }
-                                    
-                                }
-                                .listRowBackground(
-                                    RoundedRectangle(cornerRadius:16)
-                                        .fill(Color("BackgroundDocument"))
-                                        .frame(width: 358, height: 80)
-                                    
-                                        .padding(2)
-                                )
-                                
-                            }
-                            .listStyle(GroupedListStyle())
-                            .scrollContentBackground(.hidden)
-                            .background(Color("Background"))
-                            
-                            ZStack(alignment:.bottomTrailing) {
-                                HStack{
-                                    Spacer()
-                                    Button(action:{}) {
-                                        ZStack {
-                                            RoundedRectangle(cornerRadius: 16)
-                                                .fill(.red)
-                                                .frame(width: 56, height: 56)
-                                            Image(systemName: "plus")
-                                                .foregroundColor(.white)
                                         }
+                                        .padding()
                                     }
-                                    .padding()
                                 }
                             }
+                            
+                            
+                            
                         }
-                        
-                        
-                        
-                    }
-                    .toolbar(content: myTollBarContent)
+                        .toolbar(content: myTollBarContent)
+                    
                 }
-                
             }
             Spacer()
             
