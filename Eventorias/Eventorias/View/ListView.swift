3d8d1c51461953b3a5734c6ecb05eba67eda3701
Modibo KEITA <modykeita2@gmail.com>
2024-12-09 17:03:13 +0100

    update menu

--- Eventorias/Eventorias/View/ListView.swift
+++ Eventorias/Eventorias/View/ListView.swift
@@ -26,82 +26,82 @@ struct ListView: View {
                 Color("Background")
                     .ignoresSafeArea()
                 VStack {
-                  
-                        VStack(alignment: .leading) {
-                            CustomButton(listViewModel: listViewModel, tryEvent: $tryEvent)
-                                .padding()
-                            
-                            ZStack(alignment: .bottomTrailing) {
-                                List {
-                                    Section {
-                                        ForEach(listViewModel.eventEntry,id: \.self) { entry in
-                                            
-                                            HStack {
-                                                Image(entry.picture)
-                                                    .resizable()
-                                                    .frame(width: 40,height: 40)
-                                                    .padding()
-                                                
-                                                VStack(alignment:.leading){
-                                                    Text(entry.title)
-                                                        .font(.custom("Inter-Medium", size: 16))
-                                                        .lineSpacing(24 - 16)
-                                                        .fontWeight(.medium)
-                                                        .multilineTextAlignment(.leading)
-                                                        .foregroundColor(.white)
-                                                    
-                                                    Text("\(listViewModel.formatDateString( entry.dateCreation))")
-                                                        .font(.custom("Inter-Regular", size: 14))
-                                                        .lineSpacing(20 - 14)
-                                                        .fontWeight(.regular)
-                                                        .multilineTextAlignment(.leading)
-                                                        .foregroundColor(.white)
-                                                }
-                                                
-                                                Spacer()
-                                                Image(entry.poster)
-                                                    .resizable()
-                                                    .frame(width: 136, height: 80)
-                                                    .cornerRadius(12)
-                                                
-                                            }.overlay(NavigationLink(destination: {
-                                                UserDetailView(eventEntry: entry)
-                                            }, label: {
-                                                EmptyView()
-                                            }))
-                                        }
+                    
+                    VStack(alignment: .leading) {
+                        CustomButton(listViewModel: listViewModel, tryEvent: $tryEvent)
+                            .padding()
+                        
+                        ZStack(alignment: .bottomTrailing) {
+                            List {
+                                Section {
+                                    ForEach(listViewModel.eventEntry,id: \.self) { entry in
                                         
-                                    }
-                                    .listRowBackground(
-                                        RoundedRectangle(cornerRadius:16)
-                                            .fill(Color("BackgroundDocument"))
-                                            .frame(width: 358, height: 80)
-                                            .padding(2)
-                                    )
-                                    
-                                }
-                                .listStyle(GroupedListStyle())
-                                .scrollContentBackground(.hidden)
-                                .background(Color("Background"))
-                                
-                                ZStack(alignment:.bottomTrailing) {
-                                    HStack{
-                                        Spacer()
-                                        Button(action:{}) {
-                                            ZStack {
-                                                RoundedRectangle(cornerRadius: 16)
-                                                    .fill(.red)
-                                                    .frame(width: 56, height: 56)
-                                                Image(systemName: "plus")
+                                        HStack {
+                                            Image(entry.picture)
+                                                .resizable()
+                                                .frame(width: 40,height: 40)
+                                                .padding()
+                                            
+                                            VStack(alignment:.leading){
+                                                Text(entry.title)
+                                                    .font(.custom("Inter-Medium", size: 16))
+                                                    .lineSpacing(24 - 16)
+                                                    .fontWeight(.medium)
+                                                    .multilineTextAlignment(.leading)
+                                                    .foregroundColor(.white)
+                                                
+                                                Text("\(listViewModel.formatDateString( entry.dateCreation))")
+                                                    .font(.custom("Inter-Regular", size: 14))
+                                                    .lineSpacing(20 - 14)
+                                                    .fontWeight(.regular)
+                                                    .multilineTextAlignment(.leading)
                                                     .foregroundColor(.white)
                                             }
-                                        }
-                                        .padding()
+                                            
+                                            Spacer()
+                                            Image(entry.poster)
+                                                .resizable()
+                                                .frame(width: 136, height: 80)
+                                                .cornerRadius(12)
+                                            
+                                        }.overlay(NavigationLink(destination: {
+                                            UserDetailView(eventEntry: entry)
+                                        }, label: {
+                                            EmptyView()
+                                        }))
                                     }
+                                    
+                                }
+                                .listRowBackground(
+                                    RoundedRectangle(cornerRadius:16)
+                                        .fill(Color("BackgroundDocument"))
+                                        .frame(width: 358, height: 80)
+                                        .padding(2)
+                                )
+                                
+                            }
+                            .listStyle(GroupedListStyle())
+                            .scrollContentBackground(.hidden)
+                            .background(Color("Background"))
+                            
+                            ZStack(alignment:.bottomTrailing) {
+                                HStack{
+                                    Spacer()
+                                    Button(action:{}) {
+                                        ZStack {
+                                            RoundedRectangle(cornerRadius: 16)
+                                                .fill(.red)
+                                                .frame(width: 56, height: 56)
+                                            Image(systemName: "plus")
+                                                .foregroundColor(.white)
+                                        }
+                                    }
+                                    .padding()
                                 }
                             }
                         }
-                        .toolbar(content: myTollBarContent)
+                    }
+                    .toolbar(content: myTollBarContent)
                 }
             }
             
@@ -170,27 +170,38 @@ struct ListView: View {
             }
         }
     }
-//    var filter:Any{
-//       listViewModel.tryEvent(keyword: searchText)
-//    }
+    //    var filter:Any{
+    //       listViewModel.tryEvent(keyword: searchText)
+    //    }
 }
 
 
 struct CustomButton: View {
     @StateObject var listViewModel : ListViewModel
     @Binding var tryEvent : Bool
-
+    
     var body: some View {
         Menu("Sorting : \(listViewModel.FilterOption?.rawValue ?? "NONE")") {
-            ForEach(ListViewModel.FilterOption.allCases, id:\.self){ filter in
-                Button(filter.rawValue){
-                    Task{
-                        print("voici la liste filtré :\(listViewModel.FilterOption?.rawValue ?? "NONE")")
-
-                      try? await listViewModel.filterSelected(option: filter)
-       
-                    }
+            //            ForEach(ListViewModel.FilterOption.allCases, id:\.self){ filter in
+            //                Button(filter.rawValue){
+            //                    Task{
+            //                        print("voici la liste filtré :\(listViewModel.FilterOption?.rawValue ?? "NONE")")
+            //
+            //                      try? await listViewModel.filterSelected(option: filter)
+            //
+            //                    }
+            //                }
+            //            }
+            
+            
+            Button("priceHigh"){
+                Task{
+                    print("voici la liste filtré :\(listViewModel.FilterOption?.rawValue ?? "NONE")")
+                    
+                    try? await listViewModel.filterSelected(option: .priceHigh)
+                    
                 }
+                
             }
         }
     }
