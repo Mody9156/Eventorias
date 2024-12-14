52756427dddc82089aa2995f48f2786341be0c1f
Modibo KEITA <modykeita2@gmail.com>
2024-12-15 00:47:30 +0100

    update

---
+++ Eventorias/Eventorias/ViewModel/ListViewModel.swift
@@ -0,0 +1,65 @@
+//
+//  ListViewModel.swift
+//  Eventorias
+//
+//  Created by KEITA on 02/12/2024.
+//
+
+import Foundation
+import Firebase
+
+class ListViewModel : ObservableObject {
+    enum FilterOption: String, CaseIterable {
+        case noFilter
+        case date
+        case category
+    }
+    
+    
+    @Published
+    var FilterOption : FilterOption? = .noFilter
+    @Published
+    var errorMessage :String? = ""
+    @Published
+    var eventEntry : [EventEntry] = []
+    
+    private var eventoriasRepository : EventListRepresentable
+    
+    init(eventoriasRepository : EventListRepresentable = EventoriasRepository()) {
+        self.eventoriasRepository = eventoriasRepository
+    }
+    
+    func formatDateString(_ date:Date) -> String {
+        let date = Date.stringFromDate(date)
+        return date
+    }
+   
+    
+    @MainActor
+    func getAllProducts() async throws {
+        self.eventEntry = try await eventoriasRepository.getAllProducts()
+    }
+    
+    @MainActor
+    // verifier l'incrémentation
+    func filterSelected(option : FilterOption) async throws {
+        self.FilterOption = option
+        
+        switch option {
+        case .noFilter :
+            self.eventEntry = try await eventoriasRepository.getAllProducts()
+            print("Produits récupérés sans filtre. Nombre de produits : \(self.eventEntry.count)")
+            print("Données : \(self.eventEntry)")  // Affiche les données récupérées
+        case .category :
+            self.eventEntry = try await eventoriasRepository.getAllProductsSortedByCategory()
+            print("Produits récupérés triés par catégorie. Nombre de produits : \(self.eventEntry.count)")
+            print("Données : \(self.eventEntry)")  // Affiche les données récupérées
+        case .date:
+            self.eventEntry = try await eventoriasRepository.getAllProductsSortedByDate()
+            print("Produits récupérés triés par date. Nombre de produits : \(self.eventEntry.count)")
+            print("Données : \(self.eventEntry)")  // Affiche les données récupérées
+        }
+    }
+    
+   
+}
---
+++ Eventorias/Eventorias/ViewModel/UserDetailViewModel.swift
@@ -0,0 +1,55 @@
+//
+//  DetailView.swift
+//  Eventorias
+//
+//  Created by KEITA on 13/12/2024.
+//
+
+import Foundation
+
+class UserDetailViewModel: ObservableObject{
+    
+    @Published
+    var eventEntry : [EventEntry]
+    private let listViewModel : ListViewModel
+    private var googleMapView : GoogleMapView
+    
+    init(eventEntry: [EventEntry],listViewModel : ListViewModel,googleMapView : GoogleMapView) {
+        self.eventEntry = listViewModel.eventEntry
+        self.googleMapView = googleMapView
+        self.listViewModel = listViewModel
+    }
+    
+    enum Failure: Error {
+        case invalidMaps,missingAPIKey
+    }
+    
+    func formatHourString(_ hour:Date) -> String{
+        let date = Date.stringFromHour(hour)
+        return date
+    }
+    
+    func loadAPIKey() throws -> String {
+        if let path = Bundle.main.path(forResource: "Config", ofType: "plist"),
+           let config = NSDictionary(contentsOfFile: path) as? [String: Any],
+           let apiKey = config["API_KEY"] as? String {
+            return apiKey
+        }
+        throw Failure.missingAPIKey
+    }
+
+    
+    func showMapsStatic(_ Latitude:Double,_ Longitude:Double) async throws -> Data {
+        do{
+            let api =  try loadAPIKey()
+            let data = try await googleMapView.showMapsWithURLRequest(Latitude, Longitude, api)
+            print("Carte récupérée avec succès : \(data)")
+            return data
+        }catch{
+            print("Erreur lors du chargement de la carte : \(error.localizedDescription)")
+
+            throw Failure.invalidMaps
+        }
+    }
+   
+}
