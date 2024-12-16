ad41e2c51f68ad7065a1fd91390a364e84724d4a
Modibo KEITA <modykeita2@gmail.com>
2024-12-16 17:50:48 +0100

    add new view

---
+++ Eventorias/Eventorias/View/AddEventView.swift
@@ -0,0 +1,20 @@
+//
+//  AddEventView.swift
+//  Eventorias
+//
+//  Created by KEITA on 16/12/2024.
+//
+
+import SwiftUI
+
+struct AddEventView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddEventView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddEventView()
+    }
+}
