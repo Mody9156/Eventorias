91403b8cdb0feeb6f89aa38539bb54bc52b31574
Modibo KEITA <modykeita2@gmail.com>
2024-12-17 13:06:35 +0100

    refactoring

---
+++ Eventorias/Eventorias/Model/Camera/CameraManager.swift
@@ -0,0 +1,15 @@
+//
+//  CameraPreview.swift
+//  Eventorias
+//
+//  Created by KEITA on 17/12/2024.
+//
+import Foundation
+import SwiftUI
+import AVFoundation
+import Photos
+
+
+class CameraManager : NSObject, ObservableObject {}
+
+
--- Eventorias/Eventorias/ViewModel/RegistrationViewModel.swift
+++ Eventorias/Eventorias/ViewModel/RegistrationViewModel.swift
@@ -9,24 +9,6 @@ import Foundation
 import PhotosUI
 
 class RegistrationViewModel : ObservableObject {
-    @Published var capturedImage : UIImage? = nil
-    
-    private var cameraDelegate : CameraDelegate?
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
     
 }
