391269c60f82566bdee4b642e1e7e563a3b99556
Modibo KEITA <modykeita2@gmail.com>
2024-12-17 20:08:58 +0100

    update class with new do catch

--- Eventorias/Eventorias/Model/Service/Event/EventRepository.swift
+++ Eventorias/Eventorias/Model/Service/Event/EventRepository.swift
@@ -6,11 +6,29 @@
 //
 
 import Foundation
+import FirebaseFirestore
+import FirebaseFirestoreSwift
 
 public class EventRepository : ObservableObject{
     var db = Firestore.firestore().collection("eventorias")
     
-    func addNewEvent()async throws -> [EventEntry] {
-        
+    func saveToFirestore(_ event: EventEntry,completion:@escaping(Bool,Error?)-> Void ){
+      
+        do {
+                let encodedEvent = try Firestore.Encoder().encode(event)
+                
+             db.addDocument(data: encodedEvent) { error in
+                    if let error = error {
+                        print("Erreur lors de l'enregistrement de l'événement : \(error.localizedDescription)")
+                        completion(false, error)
+                    } else {
+                        print("Événement ajouté avec succès dans Firestore !")
+                        completion(true, nil)
+                    }
+                }
+            } catch let encodingError {
+                print("Erreur d'encodage : \(encodingError.localizedDescription)")
+                completion(false, encodingError)
+            }
     }
 }
