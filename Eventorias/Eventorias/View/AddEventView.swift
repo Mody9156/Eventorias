8c2cf9b67e5a019cad6192123f2b139066fef232
Modibo KEITA <modykeita2@gmail.com>
2024-12-17 18:39:36 +0100

    update function and class

--- Eventorias/Eventorias/View/AddEventView.swift
+++ Eventorias/Eventorias/View/AddEventView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import PhotosUI
+import UIKit
 
 struct AddEventView: View {
     @State var title = ""
@@ -117,22 +118,33 @@ struct CustomTexField: View {
     }
 }
 
+
 struct accessCameraView: UIViewControllerRepresentable {
     
     @Binding var selectedImage: UIImage?
     @Environment(\.presentationMode) var isPresented
 
     func makeUIViewController(context: Context) -> UIImagePickerController {
-            let imagePicker = UIImagePickerController()
+        let imagePicker = UIImagePickerController()
+        
+        // Vérifie si la caméra est disponible avant de l'utiliser
+        if !UIImagePickerController.isSourceTypeAvailable(.camera) {
+            // Si la caméra n'est pas disponible, utilise la bibliothèque photo
+            imagePicker.sourceType = .photoLibrary
+            // On peut aussi montrer une alerte ici dans le Coordinator
+            context.coordinator.showCameraUnavailableAlert()
+        } else {
+            // Si la caméra est disponible, utilise la caméra
             imagePicker.sourceType = .camera
-            imagePicker.allowsEditing = true
-            imagePicker.delegate = context.coordinator
-            return imagePicker
         }
-    
+        
+        imagePicker.allowsEditing = true
+        imagePicker.delegate = context.coordinator
+        return imagePicker
+    }
+
     func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
 
-    
     func makeCoordinator() -> CameraManager {
         return CameraManager(picker: self)
     }
--- Eventorias/Eventorias/ViewModel/Camera/CameraManager.swift
+++ Eventorias/Eventorias/ViewModel/Camera/CameraManager.swift
@@ -9,7 +9,6 @@ import SwiftUI
 import AVFoundation
 import Photos
 
-
 class CameraManager: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     var picker: accessCameraView
 
@@ -17,11 +16,28 @@ class CameraManager: NSObject, UINavigationControllerDelegate, UIImagePickerCont
         self.picker = picker
     }
 
+    func showCameraUnavailableAlert() {
+        // Création de l'alerte si la caméra n'est pas disponible
+        guard let rootViewController = UIApplication.shared.windows.first?.rootViewController else { return }
+        
+        let alert = UIAlertController(title: "Caméra non disponible", message: "La caméra n'est pas disponible sur cet appareil. Utilisez la galerie de photos.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        rootViewController.present(alert, animated: true, completion: nil)
+    }
+    
+    // Implémentation de la méthode déléguée pour sélectionner l'image
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-            guard let selectedImage = info[.originalImage] as? UIImage else { return }
-            self.picker.selectedImage = selectedImage
-            self.picker.isPresented.wrappedValue.dismiss()
+        if let image = info[.originalImage] as? UIImage {
+            self.picker.selectedImage = image
         }
+        self.picker.isPresented.wrappedValue.dismiss()
+    }
+
+    // Implémentation de la méthode déléguée pour annuler la sélection
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.picker.isPresented.wrappedValue.dismiss()
+    }
 }
 
 
