94ec2cda7fce27738779d07b6d4a90d3bf5b46da
Modibo KEITA <modykeita2@gmail.com>
2024-12-18 10:16:32 +0100

    update new function

--- Eventorias/Eventorias/ViewModel/AddEventViewModel.swift
+++ Eventorias/Eventorias/ViewModel/AddEventViewModel.swift
@@ -9,14 +9,21 @@ import Foundation
 import PhotosUI
 
 class AddEventViewModel : ObservableObject {
+    @Published var picture : String = ""
     let eventRepository: EventManagerProtocol
     
     init(eventRepository: EventManagerProtocol = EventRepository()) {
         self.eventRepository = eventRepository
     }
     
-    func saveToFirestore(_ event : EventEntry){
-        var event  = EventEntry(picture: <#T##String#>, title: <#T##String#>, dateCreation: <#T##Date#>, poster: <#T##String#>, description: <#T##String#>, hour: <#T##String#>, category: <#T##String#>, place: <#T##Address#>)
+    func saveToFirestore(picture: String, title: String, dateCreation: Date, poster: String, description: String, hour: String, category: String, street: String, city: String, postalCode: String, country: String, latitude: Double, longitude: Double  ){
+        
+        var  geoPoint =  GeoPoint(latitude: latitude, longitude: longitude)
+        
+        var adresse = Address(street: street, city: city, postalCode: postalCode, country: country, localisation: geoPoint)
+        
+        var event  = EventEntry(picture: picture, title: title, dateCreation: dateCreation, poster: poster, description: description, hour: hour, category: category, place: adresse )
+        
         eventRepository.saveToFirestore(event) { success, error in
             if success {
                 print("L'évènement a été sauvegardé avec succès.")
