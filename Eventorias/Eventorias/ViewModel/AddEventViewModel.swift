f2c4bfaa87c38231614539705f32b8304bb1f515
Modibo KEITA <modykeita2@gmail.com>
2024-12-18 10:07:52 +0100

    add new variable

--- Eventorias/Eventorias/ViewModel/AddEventViewModel.swift
+++ Eventorias/Eventorias/ViewModel/AddEventViewModel.swift
@@ -16,6 +16,7 @@ class AddEventViewModel : ObservableObject {
     }
     
     func saveToFirestore(_ event : EventEntry){
+        var event  = EventEntry(picture: <#T##String#>, title: <#T##String#>, dateCreation: <#T##Date#>, poster: <#T##String#>, description: <#T##String#>, hour: <#T##String#>, category: <#T##String#>, place: <#T##Address#>)
         eventRepository.saveToFirestore(event) { success, error in
             if success {
                 print("L'évènement a été sauvegardé avec succès.")
