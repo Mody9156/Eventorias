8b9f4032d4690c538baa28d7bd28729a9788c0d0
Modibo KEITA <modykeita2@gmail.com>
2024-12-18 12:59:19 +0100

    add new view

---
+++ Eventorias/Eventorias/Model/Protocols/Event/AddressInputView.swift
@@ -0,0 +1,20 @@
+//
+//  AddressInputView.swift
+//  Eventorias
+//
+//  Created by KEITA on 18/12/2024.
+//
+
+import SwiftUI
+
+struct AddressInputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddressInputView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddressInputView()
+    }
+}
