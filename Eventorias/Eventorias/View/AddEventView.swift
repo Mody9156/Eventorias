d2d8f6efa957faaf87e711e7a7837129249927bf
Modibo KEITA <modykeita2@gmail.com>
2024-12-18 21:00:57 +0100

    update boutton with add new comment

--- Eventorias/Eventorias/View/AddEventView.swift
+++ Eventorias/Eventorias/View/AddEventView.swift
@@ -181,22 +181,21 @@ struct AddEventView: View {
                 
                 Button(action:{
                     geocodeAddress(address: address)
-                   
+//                    if let selectedImage = savedFilePath{
+//                    let dummyImage = UIImage(contentsOfFile: selectedImage)! // Remplacez par votre UIImage
+//                    if let path = saveImageToTemporaryDirectory(image: dummyImage, fileName: "\(title)Post.jpg") {
+//                        savedFilePath = path
+//                        imageURL = URL(fileURLWithPath: path) // Convertir le chemin en URL
+//                    }
+//                }
                     
-                    if let selectedImage = selectedImage , var savedFilePath , let selected = saveImageToTemporaryDirectory(image: selectedImage, fileName: "\(title).jpg"), let latitude = coordinates?.latitude, let longitude = coordinates?.longitude{
-                        
-                        let dummyImage = UIImage(contentsOfFile: savedFilePath)!
-
-                        if let path = saveImageToTemporaryDirectory(image: dummyImage, fileName: "\(title)Post.jpg") {
-                            
-                            savedFilePath = path
-                            imageURL = URL(fileURLWithPath: path)
-                            
+                    if let selectedImage = selectedImage , let savedFilePath = savedFilePath, let selected = saveImageToTemporaryDirectory(image: selectedImage, fileName: "\(title).jpg"), let latitude = coordinates?.latitude, let longitude = coordinates?.longitude{
+                       
                             resultPicture = selected
                             var stringFromHour = String(Date.stringFromHour(hours))
                                 addEventViewModel.saveToFirestore(picture: selected, title: title, dateCreation: date, poster: savedFilePath, description: description, hour: stringFromHour, category: category, street: street, city: city, postalCode: postalCode, country: country, latitude: latitude, longitude: longitude)
                     }
-                }
+                    
                     
                 }){
                     ZStack {
