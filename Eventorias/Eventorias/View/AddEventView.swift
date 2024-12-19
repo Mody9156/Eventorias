d957013f74379d6d8046a24419a99c4ccdede34c
Modibo KEITA <modykeita2@gmail.com>
2024-12-19 17:46:15 +0100

    add scrollView

--- Eventorias/Eventorias/View/AddEventView.swift
+++ Eventorias/Eventorias/View/AddEventView.swift
@@ -43,139 +43,141 @@ struct AddEventView: View {
         ZStack {
             Color("Background")
                 .ignoresSafeArea()
-            VStack{
-                CustomTexField(text: $title, size: false, placeholder: "New event")
-                
-                CustomTexField(text: $description,size:false, placeholder: "Tap here entrer your description")
-                
-                HStack {
-                    DatePicker("", selection: $date, displayedComponents: .date)
-                        .datePickerStyle(.automatic)
-                        .foregroundColor(.white)
-                        .padding(.leading, 5)
-                        .labelsHidden()
+            ScrollView {
+                VStack{
+                    CustomTexField(text: $title, size: false, placeholder: "New event")
                     
-                    DatePicker("HH:MM", selection: $hours, displayedComponents: .hourAndMinute)
-                        .datePickerStyle(.automatic)
-                        .foregroundColor(.white)
-                        .padding(.leading, 5)
-                        .labelsHidden()
-                }
-                
-                VStack {
-                    AddressCollect(text: "Street: ", textField: $street)
-                    AddressCollect(text: "City: ", textField: $city)
-                    AddressCollect(text: "PostalCode: ", textField: $postalCode)
-                    AddressCollect(text: "country: ", textField: $country)
+                    CustomTexField(text: $description,size:false, placeholder: "Tap here entrer your description")
                     
-                }
-                .padding()
-                
-                Picker("Category", selection:$category) {
-                    ForEach(indexCategory,id:\.self){ index in
-                        Text(index)
-                            .tag(index)
+                    HStack {
+                        DatePicker("", selection: $date, displayedComponents: .date)
+                            .datePickerStyle(.automatic)
                             .foregroundColor(.white)
-                    }
-                }
-                .pickerStyle(.wheel)
-                
-                HStack(alignment: .center){
-                    Button(action:{
-                        self.showCamera.toggle()
-                    }){
-                        ZStack {
-                            Rectangle()
-                                .frame(width: 52, height:52)
-                                .foregroundColor(.white)
-                                .cornerRadius(16)
-                            
-                            Image("Camera")
-                                .foregroundColor(.black)
-                        }
-                    }
-                    .fullScreenCover(isPresented: self.$showCamera) {
-                        accessCameraView(selectedImage: self.$selectedImage)
-                            .background(.black)
+                            .padding(.leading, 5)
+                            .labelsHidden()
+                        
+                        DatePicker("HH:MM", selection: $hours, displayedComponents: .hourAndMinute)
+                            .datePickerStyle(.automatic)
+                            .foregroundColor(.white)
+                            .padding(.leading, 5)
+                            .labelsHidden()
                     }
                     
-                    PhotosPicker(selection:$selectedItems,
-                                 matching:.images) {
+                    VStack {
+                        AddressCollect(text: "Street: ", textField: $street)
+                        AddressCollect(text: "City: ", textField: $city)
+                        AddressCollect(text: "PostalCode: ", textField: $postalCode)
+                        AddressCollect(text: "country: ", textField: $country)
                         
-                        ZStack {
-                            Rectangle()
-                                .frame(width: 52, height:52)
-                                .foregroundColor(Color("Button"))
-                                .cornerRadius(16)
-                            
-                            Image("attach")
+                    }
+                    .padding()
+                    
+                    Picker("Category", selection:$category) {
+                        ForEach(indexCategory,id:\.self){ index in
+                            Text(index)
+                                .tag(index)
+                                .foregroundColor(.white)
                         }
-                    }.onChange(of: selectedItems) { newValue in
-                        for item in newValue {
-                            Task{
-                                if let data = try? await item.loadTransferable(type: Data.self), let image = UIImage(data: data){
-                                    savedFilePath = addEventViewModel.saveImageToTemporaryDirectory(image: image, fileName: "\(title).jpg")
+                    }
+                    .pickerStyle(.wheel)
+                    
+                    HStack(alignment: .center){
+                        Button(action:{
+                            self.showCamera.toggle()
+                        }){
+                            ZStack {
+                                Rectangle()
+                                    .frame(width: 52, height:52)
+                                    .foregroundColor(.white)
+                                    .cornerRadius(16)
+                                
+                                Image("Camera")
+                                    .foregroundColor(.black)
+                            }
+                        }
+                        .fullScreenCover(isPresented: self.$showCamera) {
+                            accessCameraView(selectedImage: self.$selectedImage)
+                                .background(.black)
+                        }
+                        
+                        PhotosPicker(selection:$selectedItems,
+                                     matching:.images) {
+                            
+                            ZStack {
+                                Rectangle()
+                                    .frame(width: 52, height:52)
+                                    .foregroundColor(Color("Button"))
+                                    .cornerRadius(16)
+                                
+                                Image("attach")
+                            }
+                        }.onChange(of: selectedItems) { newValue in
+                            for item in newValue {
+                                Task{
+                                    if let data = try? await item.loadTransferable(type: Data.self), let image = UIImage(data: data){
+                                        savedFilePath = addEventViewModel.saveImageToTemporaryDirectory(image: image, fileName: "\(title).jpg")
+                                    }
                                 }
                             }
                         }
                     }
-                }
-                .padding()
-                
-                if let savedFilePath = savedFilePath {
-                    Text(savedFilePath)
-                        .foregroundColor(.white)
-                }
-                //                if let selectedImage = savedFilePath, let image =  UIImage(contentsOfFile: selectedImage) {
-                //                    Image(uiImage: image)
-                //                }
-                
-                Spacer()
-                
-                Button(action:{
-                    var localisation = "\(street) + \(city) \(postalCode) \(country)"
+                    .padding()
                     
-                    addEventViewModel.geocodeAddress(address: localisation)
-                    //                    if let selectedImage = savedFilePath{
-                    //                    let dummyImage = UIImage(contentsOfFile: selectedImage)! // Remplacez par votre UIImage
-                    //                    if let path = saveImageToTemporaryDirectory(image: dummyImage, fileName: "\(title)Post.jpg") {
-                    //                        savedFilePath = path
-                    //                        imageURL = URL(fileURLWithPath: path) // Convertir le chemin en URL
-                    //                    }
+                    if let savedFilePath = savedFilePath {
+                        Text(savedFilePath)
+                            .foregroundColor(.white)
+                    }
+                    //                if let selectedImage = savedFilePath, let image =  UIImage(contentsOfFile: selectedImage) {
+                    //                    Image(uiImage: image)
                     //                }
                     
-                    if let selectedImage = selectedImage ,
-                       let savedFilePath = savedFilePath,
-                       let selected = addEventViewModel.saveImageToTemporaryDirectory(image: selectedImage,fileName: "\(title).jpg"),
-                       let latitude = addEventViewModel.coordinates?.latitude, let longitude = addEventViewModel.coordinates?.longitude{
+                    Spacer()
+                    
+                    Button(action:{
+                        var localisation = "\(street) + \(city) \(postalCode) \(country)"
                         
-                        resultPicture = selected
+                        addEventViewModel.geocodeAddress(address: localisation)
+                        //                    if let selectedImage = savedFilePath{
+                        //                    let dummyImage = UIImage(contentsOfFile: selectedImage)! // Remplacez par votre UIImage
+                        //                    if let path = saveImageToTemporaryDirectory(image: dummyImage, fileName: "\(title)Post.jpg") {
+                        //                        savedFilePath = path
+                        //                        imageURL = URL(fileURLWithPath: path) // Convertir le chemin en URL
+                        //                    }
+                        //                }
                         
-                        var stringFromHour = String(Date.stringFromHour(hours))
-                        
-                        addEventViewModel.saveToFirestore(
-                            picture: selected,
-                            title: title,
-                            dateCreation: date,
-                            poster: savedFilePath,
-                            description: description,
-                            hour: stringFromHour,
-                            category: category,
-                            street: street,
-                            city: city,
-                            postalCode: postalCode,
-                            country: country,
-                            latitude: latitude,
-                            longitude: longitude)
-                    }
-                }){
-                    ZStack {
-                        Rectangle()
-                            .frame(width: 358, height: 52)
-                            .foregroundColor(Color("Button"))
-                        
-                        Text("Validate")
-                            .foregroundColor(.white)
+                        if let selectedImage = selectedImage ,
+                           let savedFilePath = savedFilePath,
+                           let selected = addEventViewModel.saveImageToTemporaryDirectory(image: selectedImage,fileName: "\(title).jpg"),
+                           let latitude = addEventViewModel.coordinates?.latitude, let longitude = addEventViewModel.coordinates?.longitude{
+                            
+                            resultPicture = selected
+                            
+                            var stringFromHour = String(Date.stringFromHour(hours))
+                            
+                            addEventViewModel.saveToFirestore(
+                                picture: selected,
+                                title: title,
+                                dateCreation: date,
+                                poster: savedFilePath,
+                                description: description,
+                                hour: stringFromHour,
+                                category: category,
+                                street: street,
+                                city: city,
+                                postalCode: postalCode,
+                                country: country,
+                                latitude: latitude,
+                                longitude: longitude)
+                        }
+                    }){
+                        ZStack {
+                            Rectangle()
+                                .frame(width: 358, height: 52)
+                                .foregroundColor(Color("Button"))
+                            
+                            Text("Validate")
+                                .foregroundColor(.white)
+                        }
                     }
                 }
             }
