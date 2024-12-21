de06c211b86108bc837274919a84809c64c7f56b
Modibo KEITA <modykeita2@gmail.com>
2024-12-21 14:56:23 +0100

    delete function

--- Eventorias/Eventorias/ViewModel/AddEventViewModel.swift
+++ Eventorias/Eventorias/ViewModel/AddEventViewModel.swift
@@ -13,12 +13,9 @@ class AddEventViewModel : ObservableObject {
     let eventRepository: EventManagerProtocol
     @Published
      var errorMessage: String?
-    @Published
-    var coordinates : CLLocationCoordinate2D?
     
-    init(eventRepository: EventManagerProtocol = EventRepository(), coordinates : CLLocationCoordinate2D?) {
+    init(eventRepository: EventManagerProtocol = EventRepository()) {
         self.eventRepository = eventRepository
-        self.coordinates = coordinates
     }
     
     func saveToFirestore(picture: String,
@@ -51,40 +48,7 @@ class AddEventViewModel : ObservableObject {
         }
     }
     
-    @MainActor
-    func geocodeAddress(address: String) {
-        let formattedAddress = address.replacingOccurrences(of: ",", with: ",")
-            .capitalized
-        
-        guard !formattedAddress.isEmpty else {
-            self.errorMessage = "L'adresse est vide."
-            self.coordinates = nil
-            return
-        }
-
-        let geocoder = CLGeocoder()
-        geocoder.geocodeAddressString(address) { placemarks, error in
-            if let error = error { 
-                self.errorMessage = error.localizedDescription
-                self.coordinates = nil
-            } else if let placemark = placemarks?.first, let location = placemark.location {
-                let latitude = location.coordinate.latitude
-                let longitude = location.coordinate.longitude
-
-                // Vérification pour éviter les coordonnées nulles
-                if latitude == 0.0 || longitude == 0.0 {
-                    self.errorMessage = "Adresse géolocalisée avec des coordonnées invalides."
-                    self.coordinates = nil
-                } else {
-                    self.coordinates = location.coordinate
-                    self.errorMessage = nil
-                }
-            } else {
-                self.errorMessage = "Adresse introuvable."
-                self.coordinates = nil
-            }
-        }
-    }
+    
 
     
     func saveImageToTemporaryDirectory(image:UIImage, fileName:String) -> String? {
