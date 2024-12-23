734eac2a5560af076b8483236deaf645bb96ca18
Modibo KEITA <modykeita2@gmail.com>
2024-12-23 22:22:45 +0100

    update protocol

--- Eventorias/Eventorias/Model/Service/Authentification/FirebaseAuthenticationManager.swift
+++ Eventorias/Eventorias/Model/Service/Authentification/FirebaseAuthenticationManager.swift
@@ -11,7 +11,7 @@ import FirebaseFirestore
 import FirebaseCore
 
 class FirebaseAuthenticationManager :ProtocolsFirebaseData {
-    
+   
     func signIn(email: String, password: String, completion: @escaping (Result<User, Error>) -> Void) {
         Auth.auth().signIn(withEmail: email, password: password) { result, error in
             if let error = error {
@@ -47,7 +47,7 @@ class FirebaseAuthenticationManager :ProtocolsFirebaseData {
     }
 
     
-    func createUser(email: String, password: String, firtName: String, lastName: String,picture: String, completion: @escaping (Result<User, Error>) -> Void) {
+    func createUser(email: String, password: String, firtName: String, lastName: String, completion: @escaping (Result<User, Error>) -> Void) -> Void) {
         Auth.auth().createUser(withEmail: email, password: password){ result , error in
             if let error = error  {
                 completion(.failure(error))
