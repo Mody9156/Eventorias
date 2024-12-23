53b92b45307a4638b2f5c544a58adeadef3530ea
Modibo KEITA <modykeita2@gmail.com>
2024-12-23 22:18:01 +0100

    update User

--- Eventorias/Eventorias/Model/User.swift
+++ Eventorias/Eventorias/Model/User.swift
@@ -15,18 +15,21 @@ struct User : Codable, Hashable{
     let lastName: String
     let email: String
     let uid: String
+    let picture : String
 
     private enum Keys {
         static let firstName = "firstName"
         static let lastName = "lastName"
         static let email = "email"
         static let uid = "uid"
+        static let picture = "picture"
     }
 
     init?(from dictionary: [String: Any]) {
         guard let firstName = dictionary[Keys.firstName] as? String,
               let lastName = dictionary[Keys.lastName] as? String,
               let email = dictionary[Keys.email] as? String,
+              let picture = dictionary[Keys.picture] as? String,
               let uid = dictionary[Keys.uid] as? String else {
             return nil
         }
@@ -34,6 +37,7 @@ struct User : Codable, Hashable{
         self.lastName = lastName
         self.email = email
         self.uid = uid
+        self.picture = picture
     }
 
     func toDictionary() -> [String: Any] {
@@ -41,7 +45,8 @@ struct User : Codable, Hashable{
             Keys.firstName: firstName,
             Keys.lastName: lastName,
             Keys.email: email,
-            Keys.uid: uid
+            Keys.uid: uid,
+            Keys.picture: picture
         ]
     }
 }
