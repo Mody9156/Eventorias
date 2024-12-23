f8dad44b0ede3672b59baa065baf371da4ff7b2a
Modibo KEITA <modykeita2@gmail.com>
2024-12-23 13:11:24 +0100

    add new model

---
+++ Eventorias/Eventorias/Model/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Eventorias
+//
+//  Created by KEITA on 23/12/2024.
+//
+
+import Foundation
+
+struct User {
+    let firstName: String
+    let lastName: String
+    let email: String
+}
