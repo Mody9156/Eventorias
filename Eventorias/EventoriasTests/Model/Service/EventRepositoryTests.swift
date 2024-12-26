fd05583570223b10b7af265017adafc49457b963
Modibo KEITA <modykeita2@gmail.com>
2024-12-26 23:12:49 +0100

    add new function for texts unitiaire

--- Eventorias/EventoriasTests/Model/Service/EventRepositoryTests.swift
+++ Eventorias/EventoriasTests/Model/Service/EventRepositoryTests.swift
@@ -14,6 +14,26 @@ final class EventRepositoryTests: XCTestCase {
 
     func testSaveToFirestoreSuccess()  {
         let mockDb = MockFirestoreCollection()
+        mockDb.shouldSucceed = true
+        let repository = EventRepository(db: mockDb as! FirestoreCollectionProtocol)
+        let event = EventEntry(
+            picture: "https://example.com/event-picture.jpg",
+            title: "Annual Tech Conference",
+            dateCreation: Date(),
+            poster: "John Doe",
+            description: "An exciting tech conference showcasing the latest innovations in AI, blockchain, and IoT.",
+            hour: "10:00 AM - 5:00 PM",
+            category: "Technology",
+            place: Address(street: "123 Innovation Drive", city: "Techville", postalCode: "94016", country: "USA" ,localisation: GeoPoint(latitude: 37.3811, longitude: -122.3348))
+        )
+       
+        repository.saveToFirestore(event) { succes, error in
+            XCTAssertTrue(succes)
+            XCTAssert(((mockDb.capturedData?["poster"] as? String) != nil), "John Doe")
+            XCTAssert(((mockDb.capturedData?["category"] as? String) != nil), "Technology")
+        }
+        waitForExpectations(timeout:10)
+        
     }
 
 }
