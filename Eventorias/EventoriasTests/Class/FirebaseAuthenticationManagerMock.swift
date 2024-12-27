42026545bd8a1375493c54136235d41ac34280de
Modibo KEITA <modykeita2@gmail.com>
2024-12-27 14:25:48 +0100

    initialise function with right value

--- Eventorias/EventoriasTests/Class/FirebaseAuthenticationManagerMock.swift
+++ Eventorias/EventoriasTests/Class/FirebaseAuthenticationManagerMock.swift
@@ -17,4 +17,23 @@ class FirebaseAuthenticationManagerMock: FirebaseAuthenticationManager {
         completion(signResult)
     }
 
+    override func fetchUserData(userID: String, completion: @escaping (Result<User, Error>) -> Void) {
+        let userDict: [String: Any] = [
+                    "firstName": "John",
+                    "lastName": "Doe",
+                    "email": "john.doe@example.com",
+                    "uid": "12345",
+                    "picture": "http://example.com/picture.jpg"
+                ]
+        
+        //initialisation de l'object User
+        guard let user = User(from: userDict) else {
+            XCTFail("User initialization failed")
+            return
+        }
+        
+        let mockUser = user
+        completion(.success(mockUser))
+        
+    }
 }
