2609cd989af3ea4ace54e10b08f57df5973647a7
Modibo KEITA <modykeita2@gmail.com>
2024-12-27 14:37:49 +0100

    add new function

--- Eventorias/EventoriasTests/Service/FirebaseAuthenticationManagerTests.swift
+++ Eventorias/EventoriasTests/Service/FirebaseAuthenticationManagerTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import Eventorias
 final class FirebaseAuthenticationManagerTests: XCTestCase {
-
+    
     var authManager : FirebaseAuthenticationManagerMock!
     
     override func setUp() {
@@ -24,12 +24,12 @@ final class FirebaseAuthenticationManagerTests: XCTestCase {
     func testUserInitialization(){
         
         let userDict: [String: Any] = [
-                    "firstName": "John",
-                    "lastName": "Doe",
-                    "email": "john.doe@example.com",
-                    "uid": "12345",
-                    "picture": "http://example.com/picture.jpg"
-                ]
+            "firstName": "John",
+            "lastName": "Doe",
+            "email": "john.doe@example.com",
+            "uid": "12345",
+            "picture": "http://example.com/picture.jpg"
+        ]
         
         //initialisation de l'object User
         guard let user = User(from: userDict) else {
@@ -38,13 +38,61 @@ final class FirebaseAuthenticationManagerTests: XCTestCase {
         }
         
         XCTAssertEqual(user.firstName, "John")
-            XCTAssertEqual(user.lastName, "Doe")
-            XCTAssertEqual(user.email, "john.doe@example.com")
-            XCTAssertEqual(user.uid, "12345")
-            XCTAssertEqual(user.picture, "http://example.com/picture.jpg")
-         }
+        XCTAssertEqual(user.lastName, "Doe")
+        XCTAssertEqual(user.email, "john.doe@example.com")
+        XCTAssertEqual(user.uid, "12345")
+        XCTAssertEqual(user.picture, "http://example.com/picture.jpg")
+    }
     
     func testUserToDictionary() {
+        let userDict: [String: Any] = [
+            "firstName": "John",
+            "lastName": "Doe",
+            "email": "john.doe@example.com",
+            "uid": "12345",
+            "picture": "http://example.com/picture.jpg"
+        ]
+        
+        //initialisation de l'object User
+        guard let user = User(from: userDict) else {
+            XCTFail("User initialization failed")
+            return
+        }
+        
+        
+        
+        // Conversion de l'objet User en dictionnaire
+        let userDictionary = user.toDictionary()
+        
+        // Vérification que le dictionnaire est correct
+        XCTAssertEqual(userDict["firstName"] as? String, "John")
+        XCTAssertEqual(userDict["lastName"] as? String, "Doe")
+        XCTAssertEqual(userDict["email"] as? String, "john.doe@example.com")
+        XCTAssertEqual(userDict["uid"] as? String, "12345")
+        XCTAssertEqual(userDict["picture"] as? String, "http://example.com/picture.jpg")
+    }
+    
+    func testSignFailure(){
+        authManager.signResult = .failure(NSError(domain: "AuthError", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid credentials"]))
+        
+        let expectation = self.expectation(description: "signIn should complete with failure")
+        
+        authManager.signIn(email: "wrong.email@example.com", password: "wrongpassword") { result in
+            
+            switch result {
+            case .success(let user):
+                XCTFail("Expected failure but got success: \(user)")
+            case .failure(let error):
+                XCTAssertEqual(error.localizedDescription, "Invalid credentials")
+                
+            }
+            
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 1.0,handler: nil)
+    }
+    
+    func testSignSuccess(){
         let userDict: [String: Any] = [
                     "firstName": "John",
                     "lastName": "Doe",
@@ -59,38 +107,29 @@ final class FirebaseAuthenticationManagerTests: XCTestCase {
             return
         }
         
+        let mockUser = user
+        authManager.signResult = .success(mockUser)
+        let expectation = self.expectation(description: "signIn should complete successfully")
 
-           
-           // Conversion de l'objet User en dictionnaire
-           let userDictionary = user.toDictionary()
-           
-           // Vérification que le dictionnaire est correct
-           XCTAssertEqual(userDict["firstName"] as? String, "John")
-           XCTAssertEqual(userDict["lastName"] as? String, "Doe")
-           XCTAssertEqual(userDict["email"] as? String, "john.doe@example.com")
-           XCTAssertEqual(userDict["uid"] as? String, "12345")
-           XCTAssertEqual(userDict["picture"] as? String, "http://example.com/picture.jpg")
-       }
-  
-    func testSignSuccess(){
-        authManager.signResult = .failure(NSError(domain: "AuthError", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid credentials"]))
-        
-        let expectation = self.expectation(description: "signIn should complete with failure")
-        
-        authManager.signIn(email: "wrong.email@example.com", password: "wrongpassword") { result in
-            
+        authManager.signIn(email: "john.doe@example.com", password: "password") { result in
             switch result {
-            case .success(let user):
-                XCTFail("Expected failure but got success: \(user)")
-            case .failure(let error):
-                XCTAssertEqual(error.localizedDescription, "Invalid credentials")
-
-            }
-            
+                       case .success(let user):
+                           XCTAssertEqual(user.firstName, "John")
+                           XCTAssertEqual(user.lastName, "Doe")
+                           XCTAssertEqual(user.email, "john.doe@example.com")
+                           XCTAssertEqual(user.uid, "12345")
+                           XCTAssertEqual(user.picture, "http://example.com/picture.jpg")
+                       case .failure(let error):
+                           XCTFail("Expected success but got failure: \(error.localizedDescription)")
+                       }
             expectation.fulfill()
-    }
+        }
         waitForExpectations(timeout: 1.0,handler: nil)
+    }
+    
+    
+    
+    
+    
 }
-
-
 
