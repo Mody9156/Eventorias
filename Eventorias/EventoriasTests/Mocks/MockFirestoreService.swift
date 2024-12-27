8d00fe3647dba504c0fdcd79b4feb513fac54b32
Modibo KEITA <modykeita2@gmail.com>
2024-12-27 18:31:52 +0100

    initialise new file with righ function and protocol

--- Eventorias/EventoriasTests/Mocks/MockFirestoreService.swift
+++ Eventorias/EventoriasTests/Mocks/MockFirestoreService.swift
@@ -6,30 +6,35 @@
 //
 
 import XCTest
+@testable import Eventorias
 
-final class MockFirestoreService: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+class MockFirestoreService: FirestoreService {
+    var shouldSucceedFetch = true
+    var shouldSucceedSave = true
+    
+    
+    func saveUserData(userID: String, data: [String : Any], completion: @escaping (Result<Void, Error>) -> Void) {
+        if shouldSucceedFetch {
+            completion(.success(()))
+        }else{
+            completion(.failure(NSError(domain: "FirestoreError", code: -1, userInfo: [NSLocalizedDescriptionKey: "Save failed"])))
+            
         }
     }
-
+    
+    func fetchUserData(userID: String, completion: @escaping (Result<[String : Any], Error>) -> Void) {
+        if shouldSucceedFetch {
+            completion(.success([
+                "firstName": "John",
+                "lastName": "Doe",
+                "email": "john.doe@example.com",
+                "uid": userID,
+                "picture": "mockPictureURL"
+            ]))
+        } else {
+            completion(.failure(NSError(domain: "FirestoreError", code: -1, userInfo: [NSLocalizedDescriptionKey: "Fetch failed"])))
+        }
+    }
+    
+    
 }
