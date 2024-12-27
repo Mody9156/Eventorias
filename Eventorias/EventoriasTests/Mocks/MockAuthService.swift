d164aa1360fc62131f389c9c3c73ff887eadce21
Modibo KEITA <modykeita2@gmail.com>
2024-12-27 18:28:51 +0100

    add new file

--- Eventorias/EventoriasTests/Mocks/MockAuthService.swift
+++ Eventorias/EventoriasTests/Mocks/MockAuthService.swift
@@ -6,30 +6,31 @@
 //
 
 import XCTest
+@testable import Eventorias
 
-final class MockAuthService: XCTestCase {
+class MockAuthService : AuthService{
+    var shouldSucceedSign = true
+    var shouldSucceedCreateUser = true
+    
+    func signIn(email: String, password: String, completion: @escaping (Result<String, Error>) -> Void) {
+        if shouldSucceedSign {
+            completion(.success("mockUserID"))
+        }else{
+            completion(.failure(NSError(domain: "AuthError", code: -1, userInfo: [NSLocalizedDescriptionKey: "Sign-in failed"])))
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
         }
     }
+    
+    func createUser(email: String, password: String, completion: @escaping (Result<String, Error>) -> Void) {
+        if shouldSucceedSign {
+            completion(.success("mockUserID"))
+        }else{
+            completion(.failure(NSError(domain: "AuthError", code: -1, userInfo: [NSLocalizedDescriptionKey: "User creation failed"])))
+
+        }
+    }
+    
+
+    
 
 }
