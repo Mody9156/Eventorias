8c1b3d82a49511e535e12de4332309dd4aba8dcc
Modibo KEITA <modykeita2@gmail.com>
2024-12-27 10:43:45 +0100

    delete file and created new file

--- Eventorias/EventoriasTests/Mocks/MockFirestoreCollection.swift
+++ Eventorias/EventoriasTests/Mocks/MockFirestoreCollection.swift
@@ -4,28 +4,28 @@
 //
 //  Created by KEITA on 26/12/2024.
 //
-import XCTest
-
-class MockFirestoreCollection {
-    var shouldSucceed: Bool = true
-    var capturedData: [String: Any]?
-    
-    func addDocument(data: [String: Any], completion: @escaping (Error?) -> Void) {
-        capturedData = data
-        if shouldSucceed {
-            completion(nil) // Simule le succès
-        } else {
-            completion(MockFirestoreError.addDocumentFailed) // Simule une erreur
-        }
-    }
-}
-
-enum MockFirestoreError: Error {
-    case addDocumentFailed
-}
-
-protocol MockFirestoreProtocol {
-    func addDocument(data: [String: Any], completion: @escaping (Error?) -> Void)
-}
-
-extension MockFirestoreCollection: MockFirestoreProtocol {}
+//import XCTest
+//
+//class MockFirestoreCollection {
+//    var shouldSucceed: Bool = true
+//    var capturedData: [String: Any]?
+//    
+//    func addDocument(data: [String: Any], completion: @escaping (Error?) -> Void) {
+//        capturedData = data
+//        if shouldSucceed {
+//            completion(nil) // Simule le succès
+//        } else {
+//            completion(MockFirestoreError.addDocumentFailed) // Simule une erreur
+//        }
+//    }
+//}
+//
+//enum MockFirestoreError: Error {
+//    case addDocumentFailed
+//}
+//
+//protocol MockFirestoreProtocol {
+//    func addDocument(data: [String: Any], completion: @escaping (Error?) -> Void)
+//}
+//
+//extension MockFirestoreCollection: MockFirestoreProtocol {}
