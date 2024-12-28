8b93d0235ce7fe831286a7d94934845c1820b413
Modibo KEITA <modykeita2@gmail.com>
2024-12-29 00:14:39 +0100

    add  new file

--- Eventorias/EventoriasTests/Mocks/MockEventListRepresentable.swift
+++ Eventorias/EventoriasTests/Mocks/MockEventListRepresentable.swift
@@ -4,32 +4,31 @@
 //
 //  Created by KEITA on 29/12/2024.
 //
+import Foundation
+@testable import Eventorias
 
-import XCTest
+class MockEventListRepresentable: EventListRepresentable {
+    var mockProducts: [EventEntry] = []
+    var shouldThrowError = false
 
-final class MockEventListRepresentable: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func getAllProducts() async throws -> [EventEntry] {
+        if shouldThrowError {
+            throw NSError(domain: "MockError", code: -1, userInfo: nil)
         }
+        return mockProducts
     }
 
+    func getAllProductsSortedByCategory() async throws -> [EventEntry] {
+        if shouldThrowError {
+            throw NSError(domain: "MockError", code: -1, userInfo: nil)
+        }
+        return mockProducts.sorted { $0.category < $1.category }
+    }
+
+    func getAllProductsSortedByDate() async throws -> [EventEntry] {
+        if shouldThrowError {
+            throw NSError(domain: "MockError", code: -1, userInfo: nil)
+        }
+        return mockProducts.sorted { $0.dateCreation < $1.dateCreation }
+    }
 }
