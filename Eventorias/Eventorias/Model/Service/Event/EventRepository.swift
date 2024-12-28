a9d5c48408db7d2d5e6ac68844282960537f16a3
Modibo KEITA <modykeita2@gmail.com>
2024-12-28 14:28:37 +0100

    delete file

--- Eventorias/Eventorias/Model/Service/Event/EventRepository.swift
+++ Eventorias/Eventorias/Model/Service/Event/EventRepository.swift
@@ -8,19 +8,18 @@
 import Foundation
 import FirebaseFirestore
 import FirebaseFirestoreSwift
+public class EventRepository: ObservableObject, EventManagerProtocol {
+    // Remplacer FirestoreCollectionProtocol par CollectionReference directement
+    var db: CollectionReference
 
-public class EventRepository : ObservableObject,EventManagerProtocol{
-    var db : FirestoreCollectionProtocol
-    
-    init(db : FirestoreCollectionProtocol = Firestore.firestore().collection("eventorias")) {
+    // Initialisation avec une référence à la collection "eventorias"
+    init(db: CollectionReference = Firestore.firestore().collection("eventorias")) {
         self.db = db
     }
-    
-    func saveToFirestore(_ event: EventEntry,completion:@escaping(Bool,Error?)-> Void ){
-        
+
+    func saveToFirestore(_ event: EventEntry, completion: @escaping (Bool, Error?) -> Void) {
         do {
             let encodedEvent = try Firestore.Encoder().encode(event)
-            
             db.addDocument(data: encodedEvent) { error in
                 if let error = error {
                     completion(false, error)
--- Eventorias/Eventorias/View/AddEventView.swift
+++ Eventorias/Eventorias/View/AddEventView.swift
@@ -4,7 +4,6 @@
 //
 //  Created by KEITA on 16/12/2024.
 //
-
 import SwiftUI
 import PhotosUI
 import UIKit
@@ -18,30 +17,19 @@ struct AddEventView: View {
     @State private var imageURL: URL? = nil
     @State private var showCamera = false
     @State private var selectedImage: UIImage?
-    @State private var selectedImageToFile: UIImage?
-    @State var image : UIImage?
-    @State var selectedItems : [PhotosPickerItem] = []
-    @State var showAddress : Bool = false
+    @State private var address : String = ""
     @State var street : String = ""
     @State var city : String = ""
     @State var postalCode : String = ""
     @State var country : String = ""
     @State var hours = Date()
-    @State private var savedFilePath: String?
-    @State var category : String = ""
     @StateObject var addEventViewModel : AddEventViewModel
     @Environment(\.dismiss) var dismiss
-    @State var address : String = ""
     @StateObject var locationCoordinate : LocationCoordinate
     @State private var latitude : Double = 0.0
     @State private var longitude : Double = 0.0
     @State var picture = UserDefaults.standard.string(forKey: "userPicture")
     @State var file : String = ""
-    private let dateFormatter : DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.dateStyle = .medium
-        return formatter
-    }()
     @State var showFile : Bool = false
     var indexCategory = ["Music","Food","Book","Conference","Exhibition","Charity","Film"]
     @State private var selectedCategory = "Music"
@@ -52,35 +40,34 @@ struct AddEventView: View {
                 .ignoresSafeArea()
             ScrollView {
                 VStack{
-                    
                     CustomTexField(text: $title, infos: "Title", placeholder: "New event")
+                    CustomTexField(text: $description, infos: "Description", placeholder: "Tap here to enter your description")
                     
-                    CustomTexField(text: $description, infos: "Description", placeholder: "Tap here entrer your description")
-                    
-                    HStack {
-                        
-                        DatePicker("", selection: $date, displayedComponents: .date)
-                            .datePickerStyle(.automatic)
-                            .foregroundColor(.white)
-                            .padding(.leading, 5)
-                            .labelsHidden()
-                        
-                        DatePicker("", selection: $hours, displayedComponents: .hourAndMinute)
-                            .datePickerStyle(.automatic)
-                            .foregroundColor(.white)
-                            .padding(.leading, 5)
-                            .labelsHidden()
+                    VStack {
+                        HStack {
+                            DatePicker("", selection: $date, displayedComponents: .date)
+                                .datePickerStyle(.automatic)
+                                .foregroundColor(.white)
+                                .padding(.leading, 5)
+                                .labelsHidden()
+                            
+                            DatePicker("", selection: $hours, displayedComponents: .hourAndMinute)
+                                .datePickerStyle(.automatic)
+                                .foregroundColor(.white)
+                                .padding(.leading, 5)
+                                .labelsHidden()
+                        }
                     }
-                    
-                    VStack{
+
+                    VStack {
                         AddressCollect(text: "Street", textField: $street, placeholder: "Street")
                         AddressCollect(text: "City", textField: $city, placeholder: "City")
                         AddressCollect(text: "PostalCode", textField: $postalCode, placeholder: "PostalCode")
                         AddressCollect(text: "Country", textField: $country, placeholder: "Country")
                     }
                     
-                    Picker("Category", selection:$selectedCategory) {
-                        ForEach(indexCategory,id:\.self){ index in
+                    Picker("Category", selection: $selectedCategory) {
+                        ForEach(indexCategory, id: \.self) { index in
                             Text(index)
                                 .tag(index)
                                 .foregroundColor(.white)
@@ -89,137 +76,102 @@ struct AddEventView: View {
                     .pickerStyle(.segmented)
                     
                     HStack(alignment: .center){
-                        Button(action:{
+                        Button(action: {
                             self.showCamera.toggle()
-                        }){
+                        }) {
                             ZStack {
                                 Rectangle()
-                                    .frame(width: 52, height:52)
+                                    .frame(width: 72, height: 72)
                                     .foregroundColor(.white)
                                     .cornerRadius(16)
-                                
-                                Image("Camera")
+                                Image(systemName: "camera")
                                     .foregroundColor(.black)
+                                    .font(.system(size: 36))
                             }
                         }
-                        .fullScreenCover(isPresented: self.$showCamera) {
-                            accessCameraView(selectedImage: self.$selectedImage)
+                        .fullScreenCover(isPresented: $showCamera) {
+                            accessCameraView(selectedImage: $selectedImage)
                                 .background(.black)
                         }
                         
-                        Button(action:{
+                        Button(action: {
                             self.showFile.toggle()
-                        }){
+                        }) {
                             ZStack {
                                 Rectangle()
-                                    .frame(width: 52, height:52)
+                                    .frame(width: 72, height: 72)
                                     .foregroundColor(Color("Button"))
                                     .cornerRadius(16)
-                                
-                                Image("attach")
-                                
-                                Image("attach")
+                                Image(systemName: "paperclip")
                                     .foregroundColor(.black)
+                                    .font(.system(size: 36))
                             }
                         }
-                        .fullScreenCover(isPresented: self.$showFile) {
-                            accessCameraView(selectedImage: self.$selectedImageToFile)
+                        .fullScreenCover(isPresented: $showFile) {
+                            accessFilesView(selectedImage: $selectedImage)
                                 .background(.black)
                         }
-                        
                     }
-                    
-                    
-                    
                     
                     if let errorMessage = locationCoordinate.errorMessage {
                         Text(errorMessage)
                             .foregroundColor(.red)
                     }
-                    Text(address)
+                    
                     Spacer()
                     
-                    Button(action:{
-                        
-                        if street.isEmpty || city.isEmpty || postalCode.isEmpty || country.isEmpty {
-                            self.locationCoordinate.errorMessage = "Tous les champs de l'adresse doivent être remplis."
-                        } else {
-                            
-                            address = "\(street), \(city) \(postalCode), \(country)"
-                            locationCoordinate.geocodeAddress(address: address){ result in
-                                switch result {
-                                case .success(let coord):
-                                    latitude = coord.0
-                                    longitude = coord.1
-                                    print("Coordonnées récupérées : \(coord.0), \(coord.1)")
-                                    break
-                                case .failure(let error):
-                                    print("Erreur lors du géocodage : \(error.localizedDescription)")
-                                    break
-                                }
-                            }
-                            
-                            
-                            guard let selectedImage = selectedImage else {
-                                return
-                            }
-                            guard let savedFilePath = savedFilePath else {
-                                return
-                            }
-                            
-                            guard let selected = addEventViewModel.saveImageToDocumentsDirectory(image: selectedImage,fileName: "\(title).jpg") else {
-                                return
-                            }
-                            
-                            let fileURLSelected = URL(fileURLWithPath: selected)
-                            var fileURLStringSelected = fileURLSelected.absoluteString
-                            
-                            let stringFromHour = addEventViewModel.formatHourString(hours)
-                            let fileURL = URL(fileURLWithPath: savedFilePath)
-                            var fileURLString = fileURL.absoluteString
-                            
-                            
-                            if fileURLString.isEmpty && !fileURLStringSelected.isEmpty {
-                                file = fileURLStringSelected
-                            }else if !fileURLString.isEmpty && fileURLStringSelected.isEmpty {
-                                file = fileURLString
-                                
-                            }else {
-                                file = ""
-                            }
-                            
-                            if !fileURLString.isEmpty{
-                                fileURLStringSelected = ""
-                            }else {
-                                fileURLString = ""
-                            }
-                            
-                            guard let picture = picture else {
-                                return
-                            }
-                            
-                            addEventViewModel.saveToFirestore(
-                                picture: picture,
-                                title: title,
-                                dateCreation: date,
-                                poster: file,
-                                description: description,
-                                hour: stringFromHour,
-                                category: category,
-                                street: street,
-                                city: city,
-                                postalCode: postalCode,
-                                country: country,
-                                latitude: latitude,
-                                longitude: longitude)
-                            
+                    Button(action: {
+                        // Validation des champs
+                        if title.isEmpty || description.isEmpty || street.isEmpty || city.isEmpty || postalCode.isEmpty || country.isEmpty {
+                            locationCoordinate.errorMessage = "Tous les champs doivent être remplis."
+                            return
                         }
-                    }){
+                        
+                        address = "\(street), \(city) \(postalCode), \(country)"
+                        
+                        locationCoordinate.geocodeAddress(address: address) { result in
+                            switch result {
+                            case .success(let coord):
+                                latitude = coord.0
+                                longitude = coord.1
+                                print("Coordonnées récupérées : \(coord.0), \(coord.1)")
+                            case .failure(let error):
+                                print("Erreur lors du géocodage : \(error.localizedDescription)")
+                            }
+                        }
+                        
+                        guard let selectedImage = selectedImage else { return }
+                        
+                        let filePath = addEventViewModel.saveImageToDocumentsDirectory(image: selectedImage, fileName: "\(title).jpg")
+                        
+                        guard let savedFilePath = filePath else { return }
+                        
+                        let fileURL = URL(fileURLWithPath: savedFilePath)
+                        let fileURLString = fileURL.absoluteString
+                        
+                        guard let picture else {
+                            return
+                        }
+                        addEventViewModel.saveToFirestore(
+                            picture: picture,
+                            title: title,
+                            dateCreation: date,
+                            poster: fileURLString,
+                            description: description,
+                            hour: addEventViewModel.formatHourString(hours),
+                            category: selectedCategory,
+                            street: street,
+                            city: city,
+                            postalCode: postalCode,
+                            country: country,
+                            latitude: latitude,
+                            longitude: longitude)
+                        
+                    }) {
                         ZStack {
                             Rectangle()
                                 .frame(width: 358, height: 52)
                                 .foregroundColor(Color("Button"))
-                            
                             Text("Validate")
                                 .foregroundColor(.white)
                         }
@@ -232,25 +184,21 @@ struct AddEventView: View {
 }
 
 struct CustomTexField: View {
-    @Binding var text : String
-    var infos : String
-    var placeholder : String
+    @Binding var text: String
+    var infos: String
+    var placeholder: String
     var body: some View {
-        ZStack{
+        ZStack {
             Rectangle()
                 .frame(height: 56)
                 .foregroundColor(Color("BackgroundDocument"))
                 .cornerRadius(5)
-            
             VStack(alignment: .leading) {
                 Text(infos)
                     .font(.custom("Inter", size: 12))
                     .fontWeight(.regular)
                     .lineSpacing(4)
-                    .multilineTextAlignment(.leading)
-                    .textCase(.none)
                     .foregroundColor(.gray)
-                
                 TextField(placeholder, text: $text)
                     .foregroundColor(.white)
             }
@@ -260,20 +208,16 @@ struct CustomTexField: View {
 }
 
 struct accessCameraView: UIViewControllerRepresentable {
-    
     @Binding var selectedImage: UIImage?
     @Environment(\.presentationMode) var isPresented
     
     func makeUIViewController(context: Context) -> UIImagePickerController {
         let imagePicker = UIImagePickerController()
-        
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
             imagePicker.sourceType = .camera
         } else {
             imagePicker.sourceType = .photoLibrary
-            context.coordinator.showCameraUnavailableAlert()
         }
-        
         imagePicker.delegate = context.coordinator
         return imagePicker
     }
@@ -285,19 +229,13 @@ struct accessCameraView: UIViewControllerRepresentable {
     }
 }
 
-
 struct accessFilesView: UIViewControllerRepresentable {
-    
     @Binding var selectedImage: UIImage?
     @Environment(\.presentationMode) var isPresented
     
     func makeUIViewController(context: Context) -> UIImagePickerController {
         let imagePicker = UIImagePickerController()
-        
-        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
-            context.coordinator.showCameraUnavailableAlert()
-        }
-        
+        imagePicker.sourceType = .photoLibrary
         imagePicker.delegate = context.coordinator
         return imagePicker
     }
@@ -309,11 +247,10 @@ struct accessFilesView: UIViewControllerRepresentable {
     }
 }
 
-
 struct AddressCollect: View {
-    var text : String
-    @Binding var textField : String
-    var placeholder : String
+    var text: String
+    @Binding var textField: String
+    var placeholder: String
     
     var body: some View {
         ZStack {
@@ -322,30 +259,21 @@ struct AddressCollect: View {
                 .foregroundColor(Color("BackgroundDocument"))
                 .cornerRadius(5)
             VStack(alignment: .leading) {
-                
                 Text(text)
                     .font(.custom("Inter", size: 12))
                     .fontWeight(.regular)
                     .lineSpacing(4)
-                    .multilineTextAlignment(.leading)
-                    .textCase(.none)
                     .foregroundColor(.gray)
-                
                 TextField(placeholder, text: $textField)
                     .foregroundColor(.white)
             }
             .padding()
         }
     }
-    
 }
 
 struct AddEventView_Previews: PreviewProvider {
     static var previews: some View {
-        AddEventView(
-            addEventViewModel: AddEventViewModel(),
-            locationCoordinate: LocationCoordinate()
-        )
-        
+        AddEventView(addEventViewModel: AddEventViewModel(), locationCoordinate: LocationCoordinate())
     }
 }
